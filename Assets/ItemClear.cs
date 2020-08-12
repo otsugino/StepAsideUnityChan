933ee74fe992f3e0bd0efddd3813d80b986b7465
sekiya <tsugi0807@gmail.com>
2020-08-12 18:01:09 +0900

    Lesson6課題再提出
    再度、コミットを実施

---
+++ Assets/ItemClear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ItemClear : MonoBehaviour {
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+	//unityちゃんとアイテムとの距離
+	private float difference;
+
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find("unitychan");
+		//Unityちゃんとアイテムの位置（z座標）の差を求める
+		this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+		//Unityちゃんとアイテムの位置（z座標）の差が０以上（Unityちゃんが前）ならアイテムを消去
+		if (this.difference > 0)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
